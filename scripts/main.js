6ad8ce2169e1f78de606901b58003af459917567
wencaizhang <wencaistorm@foxmail.com>
2018-11-22 11:53:16 +0800

    update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,60 +1,182 @@
-layui.use(['layer', 'form', 'element', 'jquery', ], function() {
-	var element = layui.element;
-	var $ = layui.jquery;
-	//监听导航点击
-	element.on('nav(leftNav)', function(elem) {
-    if (elem[0].tagName.toLowerCase() === 'a') {
+layui.use(["layer", "form", "element", "jquery", "laytpl",], function() {
+  var element = layui.element;
+  var $ = layui.jquery;
+
+  //监听导航点击
+  element.on("nav(leftNav)", function(elem) {
+    if (elem[0].tagName.toLowerCase() === "a") {
       var navA = elem;
-      navA.parents('li').siblings('.layui-nav-itemed').removeClass('layui-nav-itemed')
+      navA
+        .parents("li")
+        .siblings(".layui-nav-itemed")
+        .removeClass("layui-nav-itemed");
     } else {
-      var navA = $(elem).find('a');
+      var navA = $(elem).find("a");
     }
-		var id   = navA.attr('data-id');
-		var url  = navA.attr('data-url');
-		var text = navA.attr('data-text');
+    var url = navA.attr("data-url");
 
-    if(!url || url === 'undefined'){ 	return; }
+    if (!url || url === "undefined") {
+      return;
+    }
 
-    var $iframe = $('#iframe');
-    if (url != $iframe.attr('src')) {
+    var $iframe = $("#iframe");
+    if (url != $iframe.attr("src")) {
       // 切换 iframe 地址
-      $iframe.attr('src', url)
+      $iframe.attr("src", url);
       locationHandler(url);
       // 修改面包屑
       breadCrumbHandler();
     }
   });
 
-  function locationHandler (url) {
-    window.location = "#" + url
+  function locationHandler(url) {
+    window.location = "#" + url;
   }
 
-  function breadCrumbHandler () {
-    var html = '<a href="">装修商家后台</a>'
+  function breadCrumbHandler() {
+    // 根据左侧菜单动态设置面包屑
+    var html = '<a href="">装修商家后台</a>';
 
-    var $this = $('#sideNav .layui-this')
+    var $this = $("#sideNav .layui-this");
     if (!$this.length) {
       return false;
     }
-    if ($this[0].tagName.toLowerCase() === 'li') {
-      var $a = $this.find('a')
+
+    if ($this[0].tagName.toLowerCase() === "li") {
+      var $a = $this.find("a");
       // html += '<a href="' + $a.attr('data-url') + '">' + $a.attr('data-text') + '</a>'
-      html += '<a >' + $a.attr('data-text') + '</a>'
+      html += "<a >" + $a.attr("data-text") + "</a>";
     } else {
       // 二级面包屑
-      var $firstSiblingA = $this.parents('.layui-nav-child').find('dd:eq(0)').find('a');
-      $firstSiblingA.attr('data-text')
-      $firstSiblingA.attr('data-url')
-      // html += '<a href="' + $firstSiblingA.attr('data-url') + '">' + $firstSiblingA.attr('data-text') + '</a>'
-      html += '<a >' + $firstSiblingA.attr('data-text') + '</a>'
+      var $firstSibling = $this.parents(".layui-nav-child").siblings("a");
+      html += "<a >" + $firstSibling.attr("data-text") + "</a>";
 
       // 三级面包屑
-      var $childA = $this.find('a');
-      // $childA.attr('data-text')
-      // $childA.attr('data-url')
-      html += '<a><cite>' + $childA.attr('data-text') + '</cite></a>'
+      var $childA = $this.find("a");
+      html += "<a><cite>" + $childA.attr("data-text") + "</cite></a>";
     }
-    $('#layui-breadcrumb').html(html)
-    element.render('breadcrumb','layui-breadcrumb')
+    $("#layui-breadcrumb").html(html);
+    element.render("breadcrumb", "layui-breadcrumb");
   }
+
+  function initIframeSrc() {
+    var url = window.location.hash.split("#")[1];
+    if (!url) {
+      return false;
+    }
+
+    // 修改 iframe 路径
+    $("#iframe").attr("src", url);
+
+    // 对左侧菜单展开和选中状态进行修正
+    var checkedItem = menuList.find(function(item) {
+      if (item.href === url) {
+        return true;
+      }
+
+      if (item.children && item.children.length) {
+        var flag = item.children.find(function(item2) {
+          return item2.href === url;
+        });
+        if (flag) {
+          return true;
+        }
+      }
+    });
+
+    // 取消全部的高亮状态
+    menuList.map(function(item) {
+      item.checked = false;
+      item.this = false;
+      if (item.children && item.children.length) {
+        item.children.map(function(item2) {
+          item2.checked = false;
+          item2.this = false;
+        });
+      }
+    });
+
+    // 高亮对应的
+    checkedItem.itemed = true;
+    if (checkedItem.href === url) {
+      checkedItem.this = true;
+    } else {
+      checkedItem.children.forEach(function(item) {
+        if (item.href === url) {
+          item.this = true;
+        }
+      });
+    }
+  }
+
+  var laytpl = layui.laytpl;
+  var menuList = [
+    { title: "首页", href: "./pages/home/index.html", this: true },
+    {
+      title: "交易管理",
+      children: [
+        {
+          title: "订单管理",
+          href: "./pages/deal/order.html"
+        }
+      ]
+    },
+    {
+      title: "店铺管理",
+      children: [
+        {
+          title: "基本设置",
+          href: "./pages/system.html"
+        },
+        {
+          title: "案例设置",
+          href: "./pages/system.html"
+        },
+        {
+          title: "广告管理",
+          href: "./pages/system.html"
+        },
+        {
+          title: "店铺资质",
+          href: "./pages/system.html"
+        }
+      ]
+    },
+    {
+      title: "客户服务",
+      children: [
+        {
+          title: "投诉管理",
+          href: "./pages/system.html"
+        },
+        {
+          title: "问答-待回答",
+          href: "./pages/system.html"
+        },
+        {
+          title: "问答-已回答",
+          href: "./pages/system.html"
+        }
+      ]
+    },
+    { title: "数据中心" },
+    {
+      title: "角色管理",
+      children: [
+        { title: "工长", href: "./pages/system.html" },
+        { title: "设计师", href: "./pages/system.html" }
+      ]
+    },
+    { title: "账号管理" }
+  ];
+
+  initIframeSrc();
+  var getTpl = sideNavTemplate.innerHTML;
+  var view = document.getElementById("sideNav");
+  laytpl(getTpl).render(menuList, function(html) {
+    // 渲染左侧菜单
+    view.innerHTML = html;
+    element.render('nav', 'leftNav')
+    breadCrumbHandler();
+  });
 });
