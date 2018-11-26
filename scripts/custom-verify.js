4da3c0f35db2ce982b0b6cd8ec5809ed2fc93e98
wencaizhang <1052642137@qq.com>
2018-11-26 15:07:14 +0800

    udpate phone reg

--- scripts/custom-verify.js
+++ scripts/custom-verify.js
@@ -38,6 +38,12 @@ function getCustomVerifyObj() {
 			}
 		},
 		myphone: function (value, dom) {
+			if (value == '') { return false; }
+			var reg = /^(13[0-9]|14[579]|15[012356789]|17[135678]|18[0-9])[0-9]{8}$/;
+			if (reg.test(value)) {
+				return '请输入正确的手机号';
+			}
+
 			var phoneBlankList = [
 				'01234567891',
 				'12345678910',
